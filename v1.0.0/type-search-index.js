f261f5dc41a35544c858afaca9d9eaef11761efb
Bonajo <Bonajo@users.noreply.github.com>
2025-10-27 16:49:46 +0000

    Deploying to javadoc from @ FontysVenlo/alda_simpletime_api@5297e2b7ca151ca83b5deda6fb6c28c39619180d 🚀

---
+++ v1.0.0/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"simpletimeapi","l":"AbstractSimpleTimeFactory"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"simpletimeapi","l":"Duration"},{"p":"simpletimeapi","l":"Time"}];updateSearchResults();
